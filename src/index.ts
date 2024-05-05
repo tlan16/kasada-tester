7ff0218d66b1d3cdfa2a7a78189430416eaefc58
Frank Lan <frank.lan@rea-group.com>
2024-05-05 22:30:02 +1000

    firefox

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,10 @@
-import { chromium } from 'playwright';
-import { newInjectedContext } from 'fingerprint-injector';
+import { firefox } from 'playwright';
 
 (async () => {
-  const browser = await chromium.launch({
-    channel: 'chrome',
+  const context = await firefox.launch( {
     headless: false,
   });
-  const context = await newInjectedContext(
-    browser,
-    {
-      // Constraints for the generated fingerprint (optional)
-      fingerprintOptions: {
-        devices: ['mobile'],
-        operatingSystems: ['macos'],
-      },
-      // Playwright's newContext() options (optional, random example for illustration)
-      newContextOptions: {
-        geolocation: {
-          latitude: -37.840935,
-          longitude: 144.946457,
-        },
-      },
-    },
-  );
 
   const page = await context.newPage();
-  await page.goto('https://www.property.com.au/');
+  await page.goto('https://www.property.com.au/vic/mount-waverley-3149/sesame-st/12-pid-3945769/');
 })();
